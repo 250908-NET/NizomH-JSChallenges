b92399ee76f43984dc05049eab2a72b2e5221381
Husan Nizom <husan.dev@h-n.tech>
2025-10-09 10:51:10 -0400

    completed task 21

--- script.js
+++ script.js
@@ -6,4 +6,6 @@ const squares = nums.map(num => square(num));
 const numbers = [10, 15, 20, 25, 30];
 const evens = numbers.filter(num => num % 2 == 0);
 
-console.log(evens);
+const makeBook = (title, author) => ({title, author});
+
+console.log(makeBook("Harry Potter", "JKR"));
