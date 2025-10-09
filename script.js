389010a2e50cedd27793bf3c72d9bf516860d93d
Husan Nizom <husan.dev@h-n.tech>
2025-10-09 12:03:47 -0400

    completed task 26

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 document.getElementById('fetchBtn').addEventListener('click', () => {
   const id = document.getElementById('postId').value;
   fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-    .then(res => res.json())
+    .then(res => {
+      if (!res.ok) throw new Error('Network response was not ok');
+      return res.json();
+    })
     .then(data => {
       document.getElementById('output').innerHTML = `<h2>${data.title}</h2><p>${data.body}</p>`;
-    });
+    })
+    .catch(error => {
+     console.error('Fetch error:', error);
+     document.getElementById('output').innerHTML = 'No post found.';
+    }); 
 });
