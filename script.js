3651dbf2e0dd8b7a98d59a6869228937c97803bc
Husan Nizom <husan.dev@h-n.tech>
2025-10-09 11:49:55 -0400

    completed task 24

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then(response => (
-  response.json()
-))
-.then(posts => {
-  posts.slice(0, 5).forEach(post => {
-    const item = document.createElement('div');
-    item.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
-    document.body.appendChild(item);
-  }); 
-})
+document.getElementById('loadBtn').addEventListener('click', () => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => (
+    response.json()
+  ))
+  .then(posts => {
+    posts.slice(0, 5).forEach(post => {
+      const item = document.createElement('div');
+      item.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
+      document.body.appendChild(item);
+    }); 
+  })
+});
